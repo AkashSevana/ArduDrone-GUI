45a06d15485d9a12871b11383b14d1e1920a0ff5
ASevana <aku123sevana@gmail.com>
2018-09-30 02:33:43 +0530

    Added Google maps support
    Google maps added

--- src/com/drone/application/gui/MainScreenController.java
+++ src/com/drone/application/gui/MainScreenController.java
@@ -15,7 +15,20 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import com.drone.application.gui.utils.DataSource;
+import com.lynden.gmapsfx.GoogleMapView;
 import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.DirectionsPane;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.object.Marker;
+import com.lynden.gmapsfx.javascript.object.MarkerOptions;
+import com.lynden.gmapsfx.service.directions.DirectionStatus;
+import com.lynden.gmapsfx.service.directions.DirectionsResult;
+import com.lynden.gmapsfx.service.directions.DirectionsService;
+import com.lynden.gmapsfx.service.directions.DirectionsServiceCallback;
+import com.lynden.gmapsfx.util.MarkerImageFactory;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -28,11 +41,18 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class MainScreenController implements Initializable, MapComponentInitializedListener {
+public class MainScreenController implements Initializable, MapComponentInitializedListener, DirectionsServiceCallback {
 	
 	final String RESOURCE_PATH = "resources/";
 	
 	protected static DataSource droneparameters;
+	protected GoogleMap map;
+	protected DirectionsService directionsService;
+	protected DirectionsPane directionsPane;
+	protected Marker DroneMarker;
+	protected LatLong takeOff;
+	@FXML
+	protected GoogleMapView mapView;
 	
 	public DataSource getDroneparameters() {
 		return MainScreenController.droneparameters;
@@ -45,13 +65,15 @@ public class MainScreenController implements Initializable, MapComponentInitiali
 
 
 	@FXML
-	BorderPane MainScreen;
+	protected BorderPane MainScreen;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		System.out.println("loading default datasource");
 		droneparameters = new DataSource();
 		System.out.println("params loaded");
+		//mapView.setKey("AIzaSyKwTCISuW2YXt-376JYLIxaX2_imQLuA20");
+		mapView.addMapInitializedListener(this);
 	}
 	
 	
@@ -146,6 +168,33 @@ public class MainScreenController implements Initializable, MapComponentInitiali
 
 	@Override
 	public void mapInitialized() {
+		
+		 MapOptions mapOptions = new MapOptions();
+		    mapOptions.center(new LatLong(41.121086, 14.181095))
+		            .mapType(MapTypeIdEnum.ROADMAP)
+		            .overviewMapControl(false)
+		            .panControl(false)
+		            .rotateControl(false)
+		            .scaleControl(false)
+		            .streetViewControl(false)
+		            .zoomControl(false)
+		            .zoom(12);
+		    
+		    map = mapView.createMap(mapOptions);
+		    //Add a marker to the map
+		    MarkerOptions markerOptions = new MarkerOptions();
+		    takeOff = new LatLong(41.121086, 14.181095);
+		    System.out.println("markers");
+		    markerOptions.position(takeOff)
+		                .visible(Boolean.TRUE)
+		                .title("TakeOff");
+		    DroneMarker = new Marker( markerOptions);
+		    map.addMarker(DroneMarker);
+	}
+
+
+	@Override
+	public void directionsReceived(DirectionsResult arg0, DirectionStatus arg1) {
 		
 	}
 }
